98ae16addecfd17b1657cb485c5a82af2ec1a989
Federico Voges <federico@puppet.com>
2017-06-16 00:31:18 +0200

    add mco shell agent

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,13 @@
+# class profile::base
 class profile::base {
 
-  include ::pe_mco_shell_agent
+  if $::facts['pe_server_version'] {
+    $install_app = true
+  } else {
+    $install_app = false
+  }
 
+  class { '::pe_mco_shell_agent':
+    install_app => $install_app,
+  }
 }
