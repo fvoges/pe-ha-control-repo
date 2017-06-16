3ee69aa289c9bb8785bd9e6da32929c528921b10
Federico Voges <federico@puppet.com>
2017-06-16 10:07:59 +0200

    add base packages, datacentre external fact and refactor install_app logic

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,13 +1,44 @@
 # class profile::base
 class profile::base {
+  $packages = hiera("profile::base::packages_${::facts['os']['family'].downcase}")
 
-  if $::facts['pe_server_version'] {
-    $install_app = true
-  } else {
-    $install_app = false
+  assert_type(Hash, $packages)
+
+  # We only want to install the mco app on the Puppet Masters
+  $install_app = $::facts['pe_server_version'] ? {
+    undef   => false,
+    default => true,
+  }
+
+  $datacentre = $::facts['networking']['hostname'] ? {
+    /01/    => 'dc01',
+    /02/    => 'dc02',
+    default => 'unknown',
+  }
+
+  $packages.each | String $package, Hash $attrs | {
+    package { $package:
+      ensure   => $attrs['ensure'],
+      provider => $attrs['provider'],
+    }
   }
 
   class { '::pe_mco_shell_agent':
     install_app => $install_app,
+  }
+
+  file { ['/etc/puppetlabs/facter', '/etc/puppetlabs/facter/facts.d']:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/etc/puppetlabs/facter/facts.d/org_datacentre.txt':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "org_datacentre=${datacentre}\n",
   }
 }
