b2ea8fa44009e11307c6656d30535adff3715ba7
Federico Voges <federico@puppet.com>
2017-06-16 02:12:48 +0200

    fix fact names

--- site/profile/manifests/puppet/compile.pp
+++ site/profile/manifests/puppet/compile.pp
@@ -4,7 +4,7 @@ class profile::puppet::compile {
   @@haproxy::balancermember { "puppet_${::certname}":
     listening_service => 'puppet00',
     server_names      => $::facts['networking']['hostname'],
-    ipaddresses       => $::facts['networking']['ipaddress'],
+    ipaddresses       => $::facts['networking']['ip'],
     ports             => '8140',
     options           => 'check',
   }
@@ -12,7 +12,7 @@ class profile::puppet::compile {
   @@haproxy::balancermember { "orchestrator_${::certname}":
     listening_service => 'orchestrator00',
     server_names      => $::facts['networking']['hostname'],
-    ipaddresses       => $::facts['networking']['ipaddress'],
+    ipaddresses       => $::facts['networking']['ip'],
     ports             => '8142',
     options           => 'check',
   }
